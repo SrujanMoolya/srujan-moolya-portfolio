cfb181e10a72063b8f36f33cc8297ff8c8e6073a
SVVAAP <workwithsvvaap@gmail.com>
2025-09-19 13:14:04 +0530

    update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -350,13 +350,14 @@ const Projects = () => {
           </div>
           <div className="flex flex-wrap justify-center gap-6 items-center">
             {/* Company logos - use reliable SVG/PNG URLs */}
-            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" alt="Google" className="h-10 w-auto grayscale hover:grayscale-0 transition" title="Google" />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg" alt="Microsoft" className="h-10 w-auto grayscale hover:grayscale-0 transition" title="Microsoft" />
+            <img src="https://chipsyservices.com/wp-content/uploads/2024/09/Group-1.png" alt="Google" className="h-10 w-auto grayscale hover:grayscale-0 transition" title="Google" />
+           
             
-            <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png" alt="Meta" className="h-10 w-auto grayscale hover:grayscale-0 transition bg-white rounded-full p-1" title="Meta" />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt="Apple" className="h-10 w-auto grayscale hover:grayscale-0 transition" title="Apple" />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg" alt="Tesla" className="h-10 w-auto grayscale hover:grayscale-0 transition" title="Tesla" />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix" className="h-10 w-auto grayscale hover:grayscale-0 transition" title="Netflix" />
+            <img src="https://assets.partnerfleet.app/variants/ksyfvjj1a6qrq7pmhq2q72rpgk2i/39cb74b2619edb3d629a9a72e177351c3a50e92488c1088f81e621fda9b2aff8" alt="Meta" className="h-20 w-auto grayscale hover:grayscale-0 transition bg-white rounded-full p-1" title="Meta" />
+             <img src="https://www.cognizant.com/us/media_1808da395be9f77c0124de824530b0338915414a8.svg" alt="Microsoft" className="h-10 w-auto grayscale hover:grayscale-0 transition" title="Microsoft" />
+            <img src="https://polkadot.com/_next/static/media/polkadot-logo.0e1e2c79.png" alt="Apple" className="h-10 w-auto grayscale hover:grayscale-0 transition" title="Apple" />
+            <img src="https://s3-us-west-2.amazonaws.com/cbi-image-service-prd/original/497f6539-c32e-404a-9ec3-d0dd205f37cd.png" alt="Tesla" className="h-20 w-auto grayscale hover:grayscale-0 transition" title="Tesla" />
+            <img src="https://hirepro.in/wp-content/uploads/2025/05/HirePro-logo.svg" alt="Netflix" className="h-10 w-auto grayscale hover:grayscale-0 transition" title="Netflix" />
             <img src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="Amazon" className="h-10 w-auto grayscale hover:grayscale-0 transition bg-white rounded p-1" title="Amazon" />
             <span className="text-2xl font-bold text-muted-foreground">...</span>
           </div>
